928437cf02cc94e104eca702ca9dcdf20c2dcf59
Joao Gripp <corguinha@gmail.com>
2023-03-21 11:56:55 -0300

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Joao Gripp on 21/03/23.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
