45b4813a754c65d3e576475f62ca26aa56a8c9bf
Joao Gripp <corguinha@gmail.com>
2023-03-23 09:20:37 -0300

    OrderView Screen UI and swipe delete implemented

--- Appetizers/Screens/OrderView/OrderView.swift
+++ Appetizers/Screens/OrderView/OrderView.swift
@@ -8,11 +8,33 @@
 import SwiftUI
 
 struct OrderView: View {
+    
+    @State private var orderItems = MockData.orderItems
+    
     var body: some View {
         NavigationView {
-            Text("Order View")
-                .navigationTitle("😇 Orders")
+            VStack {
+                List {
+                    ForEach(MockData.orderItems) { appetizer in
+                        AppetizerListCell(appetizer: appetizer)
+                    }
+                    .onDelete(perform: deleteItems)
+                }
+                .listStyle(PlainListStyle())
+                
+                Button {
+                    print("$99.99 - order Place")
+                } label: {
+                    APButton(title: "Place Order")
+                }
+                .padding(.bottom, 25)
+            }
+            .navigationTitle("😇 Orders")
         }
+    }
+    
+    func deleteItems(at offsets: IndexSet) {
+        orderItems.remove(atOffsets: offsets)
     }
 }
 
