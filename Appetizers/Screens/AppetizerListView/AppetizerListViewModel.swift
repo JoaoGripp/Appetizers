bb25c331cdd41148887c4c8de26bcc5f1b8f2f1d
Joao Gripp <corguinha@gmail.com>
2023-03-26 10:00:19 -0300

    Update iOS 15 - Async

--- Appetizers/Screens/AppetizerListView/AppetizerListViewModel.swift
+++ Appetizers/Screens/AppetizerListView/AppetizerListViewModel.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-final class AppetizerListViewModel: ObservableObject {
+@MainActor final class AppetizerListViewModel: ObservableObject {
     @Published var appetizers: [Appetizer] = []
     @Published var alertItem: AlertItem?
     @Published var isLoading = false
@@ -17,30 +17,58 @@ final class AppetizerListViewModel: ObservableObject {
     
     func getAppetizers() {
         isLoading = true
-        NetworkManager.shared.getAppetizers { result in
-            DispatchQueue.main.async {
-                self.isLoading = false
-                switch result {
-                case .success(let appetizers):
-                    self.appetizers = appetizers
-                case .failure(let error):
-                    switch error {
-                    case .invalidData:
-                        self.alertItem = AlertContext.invalidData
-                        
+        
+        Task {
+            do {
+                appetizers = try await NetworkManager.shared.getAppetizers()
+                isLoading = false
+            } catch {
+                if let apError = error as? APError {
+                    switch apError {
                     case .invalidURL:
-                        self.alertItem = AlertContext.invalidURL
-                        
+                        alertItem = AlertContext.invalidURL
                     case .invalidResponse:
-                        self.alertItem = AlertContext.invalidResponse
-                        
+                        alertItem = AlertContext.invalidResponse
+                    case .invalidData:
+                        alertItem = AlertContext.invalidData
                     case .unableToComplete:
-                        self.alertItem = AlertContext.unableToComplete
+                        alertItem = AlertContext.unableToComplete
                     }
+                } else {
+                    alertItem = AlertContext.invalidResponse // generic error
                 }
+                isLoading = false
             }
         }
+        
     }
+    
+    //    func getAppetizers() {
+    //        isLoading = true
+    //        NetworkManager.shared.getAppetizers { result in
+    //            DispatchQueue.main.async {
+    //                self.isLoading = false
+    //                switch result {
+    //                case .success(let appetizers):
+    //                    self.appetizers = appetizers
+    //                case .failure(let error):
+    //                    switch error {
+    //                    case .invalidData:
+    //                        self.alertItem = AlertContext.invalidData
+    //
+    //                    case .invalidURL:
+    //                        self.alertItem = AlertContext.invalidURL
+    //
+    //                    case .invalidResponse:
+    //                        self.alertItem = AlertContext.invalidResponse
+    //
+    //                    case .unableToComplete:
+    //                        self.alertItem = AlertContext.unableToComplete
+    //                    }
+    //                }
+    //            }
+    //        }
+    //    }
+    //}
 }
-
 
